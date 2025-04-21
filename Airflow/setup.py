b8e9eab2a29a391b624fb20131e201b2bbf245e2
Gabz <gabs.ferr08@gmail.com>
2025-04-21 01:49:20 -0300

    Update setup.py

--- Airflow/setup.py
+++ Airflow/setup.py
@@ -1,14 +1,27 @@
 import os, socket
+import subprocess as sub
 
 def init():
     #Ta dando errado 
     print("Iniciando banco de dados e criando administrador\n")
-    os.system("docker compose run --rm airflow-init")
+    #Talvez colocar o path para ele saber onde tem que trabalhar???
+    ini = sub.run(args=["docker", "compose", "run", "--rm", "airflow-init"], capture_output=True, text=True)
+
+    if ini.returncode == 0:
+        print(f"- {ini.stdout}")
+    else:
+        print(f"- Erro: {ini.stderr}")
+    
+    #Continua dando errado, mas agora tem uns passos a mais
+
+    print(ini)
+    #os.system("docker compose run --rm airflow-init")
 
 def comp():
     #Ta dando errado 2
     print("Criando ambiente do Airflow\n")
-    os.system("docker compose up -d")
+    #Path aqui também?
+    #os.system("docker compose up -d")
 
 def ip():
     hostname = socket.gethostname()
@@ -16,16 +29,26 @@ def ip():
     return ip
 
 def main():
-    #Inicialização de banco de dados e criação de adm
-    init()
-    #Inicia a interface do Airflow
-    comp()
-    
-    #Aqui é só pra flr que deu td certo e mostrar os endereços para acessar
-    print(f"\nAirflow iniciado \
-           \nAcesse colocando os seguintes endereços no navegador: \
-           \nMesma máquina: http://localhost:9090 \
-           \nOutra máquina: http://{ip()}:9090\n")
+    doc = sub.run(args=["docker", "info"], capture_output=True)
+
+    if doc.returncode == 0:
+        if init():
+
+            '''
+            #Ver se o init deu certo tbm antes de vir pra cá
+            #Inicia a interface do Airflow
+            comp()
+
+            #E só se der td certo ele mostra isso
+            #Aqui é só pra flr que deu td certo e mostrar os endereços para acessar
+            print(f"\nAirflow iniciado \
+                   \nAcesse colocando os seguintes endereços no navegador: \
+                   \nMesma máquina: http://localhost:9090 \
+                   \nOutra máquina: http://{ip()}:9090\n")
+            '''
+
+    else:
+        print("Docker não esta funcionando")
 
 
 if __name__ == "__main__":
