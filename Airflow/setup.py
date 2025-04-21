7f8b5640770c53a0bca6c8a3b0f3189ea7bfb873
Gabz <gabs.ferr08@gmail.com>
2025-04-21 00:13:05 -0300

    Atualizações do setup

--- Airflow/setup.py
+++ Airflow/setup.py
@@ -1,18 +1,31 @@
-import os
-
+import os, socket
 
 def init():
-    print("Iniciando banco de dados e criando administrador")
+    #Ta dando errado 
+    print("Iniciando banco de dados e criando administrador\n")
     os.system("docker compose run --rm airflow-init")
 
 def comp():
-    print("Criando ambiente do Airflow")
+    #Ta dando errado 2
+    print("Criando ambiente do Airflow\n")
     os.system("docker compose up -d")
 
+def ip():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    return ip
+
 def main():
+    #Inicialização de banco de dados e criação de adm
     init()
+    #Inicia a interface do Airflow
     comp()
-    print("")
+    
+    #Aqui é só pra flr que deu td certo e mostrar os endereços para acessar
+    print(f"\nAirflow iniciado \
+           \nAcesse colocando os seguintes endereços no navegador: \
+           \nMesma máquina: http://localhost:9090 \
+           \nOutra máquina: http://{ip()}:9090\n")
 
 
 if __name__ == "__main__":
