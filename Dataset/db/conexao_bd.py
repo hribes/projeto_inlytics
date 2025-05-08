05cf3bd38865442d3d05e73cd3594d5eac281cbe
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-05-08 15:50:06 -0300

    adicionei uma função para conectar-se ao BD
    adicionei o __init__ para criar um módulo de db

--- Dataset/db/conexao_bd.py
+++ Dataset/db/conexao_bd.py
@@ -1,34 +1,22 @@
-import mysql.connector
-from mysql.connector import errorcode
+from mysql.connector import connect, errorcode, Error
 
-conn = None
+def conectar_db():
+		conn = None
 
-# faz a conexão mais a checagem pra ver se está conectado
-try:
-    conn = mysql.connector.connect(
-        user= "",
-        password= "",
-        database= "",
-        host= ""
-    )
-except mysql.connector.Error as err:
-    if(err.errno == errorcode.ER_ACCESS_DENIED_ERROR):
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
-    # else:
-    #     conn.close()
-    
-# faz a consulta
-with conn.cursor() as cursor:
-	#  manda pro banco de dados a consulta
-    consulta = cursor.execute("SELECT * FROM rfm LIMIT 5")
-# pega o que foi pedido
-    rows = cursor.fetchall()
-    
-    print(rows)
-# mostra o resultado
-    for rows in rows:
-        print(rows)
+		# faz a conexão mais a checagem pra ver se está conectado
+		try:
+			conn = connect(
+				user= "",
+				password= "",
+				database= "",
+				host= ""
+			)
+		except Error as err:
+			if(err.errno == errorcode.ER_ACCESS_DENIED_ERROR):
+				print("Something is wrong with your username or password")
+			elif err.errno == errorcode.ER_BAD_DB_ERROR:
+				print("Database does not exist")
+			else:
+				print(err)
+
+		return conn
