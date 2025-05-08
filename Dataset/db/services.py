3f52dee1f9729915ae9df48cd1c315e9d3f53696
Paola Biscaro <paolabiscaroper@gmail.com>
2025-05-08 20:03:20 -0300

    Correção do erro do arquivo services.py

--- Dataset/db/services.py
+++ Dataset/db/services.py
@@ -1,6 +1,7 @@
 from conexao_bd import conectar_db 
 
 def list_all_customers():
+    conn = conectar_db()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM customer")
     customer = cursor.fetchall()
