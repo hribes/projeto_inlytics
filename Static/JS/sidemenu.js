6e496a7f75e82aa50802039f1b2f31cbeea8ddda
lorenasouza14 <souzaloro107@gmail.com>
2025-06-04 09:52:08 -0300

    Ajustando a tela de churn, sazonalidade, usuário e adicionando o js na barra de pesquisa

--- Static/JS/sidemenu.js
+++ Static/JS/sidemenu.js
@@ -11,3 +11,42 @@ const body = document.querySelector("body"),
 function toggleConteudo(el) {
     el.classList.toggle("ativo");
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
+
+    searchInput.addEventListener('input', function () {
+        const filtro = searchInput.value.toLowerCase();
+        const clientes = document.querySelectorAll('.div-clique');
+
+        clientes.forEach(cliente => {
+            const texto = cliente.innerText.toLowerCase();
+
+            if (texto.includes(filtro)) {
+                cliente.style.display = 'block';
+            } else {
+                cliente.style.display = 'none';
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
+
+    searchInput.addEventListener('input', function () {
+        const filtro = searchInput.value.toLowerCase();
+        const churnClientes = document.querySelectorAll('.churn-informacoes');
+
+        churnClientes.forEach(cliente => {
+            const texto = cliente.innerText.toLowerCase();
+
+            if (texto.includes(filtro)) {
+                cliente.style.display = 'flex'; // ou 'block', dependendo do layout
+            } else {
+                cliente.style.display = 'none';
+            }
+        });
+    });
+});
+
